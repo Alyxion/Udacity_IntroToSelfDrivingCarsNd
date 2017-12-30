51d1fa3aa7deea34f69215b99d4f0ecf20a9c3e3
Michael Ikemann <ikemann@logiball.de>
2017-12-30 12:18:35 +0100

    .

--- 5_3_Optimize_Histogram_Filter_Cpp/headers/move_improved.h
+++
@@ -1,9 +0,0 @@
-#ifndef MOVE_IMPROVED_H
-#define MOVE_IMPROVED_H
-
-#include <vector>
-#include <iostream>
-
-std::vector< std::vector <float> > move_improved(int dy, int dx, std::vector < std::vector <float> > &beliefs); 
-
-#endif /* MOVE_IMPROVED.H */
